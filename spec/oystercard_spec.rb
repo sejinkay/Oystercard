ce64410f6ab007afe46da2cfd03d66099c899537
sejinkay <sejinkay@gmail.com>
2019-04-08 15:35:53 +0100

    added top_up function

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,13 @@ describe Oystercard do
   it 'checks the card balance returns 0' do
     expect(subject.balance).to eq 0
   end
+
+  describe '#top_up' do
+    it 'adds value to the balance' do
+      expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+    end
+    it 'adds value to the balance' do
+      expect{ subject.top_up 15 }.to change{ subject.balance }.by 15
+    end
+  end
 end
